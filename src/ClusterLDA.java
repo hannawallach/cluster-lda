1d2df8ae3c8f936c30f0952add21a65369013906
Hanna M. Wallach <wallach@cs.umass.edu>
2011-07-12 22:19:01 -0400

    Added (untested) code for LDA with (fixed) document clusters.

---
+++ src/ClusterLDA.java
@@ -0,0 +1,197 @@
+package edu.umass.cs.wallach.cluster;
+
+import java.io.*;
+import java.util.*;
+
+import gnu.trove.*;
+
+import cc.mallet.types.*;
+import cc.mallet.util.*;
+
+public class ClusterLDA {
+
+  // observed counts
+
+  private WordScore wordScore;
+  private ClusterTopicScore topicScore;
+
+  private int W, T, D, C; // constants
+
+  private int[][] z; // topic assignments
+
+  private LogRandoms rng; // random number generator
+
+  private double getScore(int w, int j, int d, int c) {
+
+    return wordScore.getScore(w, j) * topicScore.getScore(j, d, c);
+  }
+
+  // computes P(w, z) using the predictive distribution
+
+  private double logProb(Corpus docs) {
+
+    double logProb = 0;
+
+    wordScore.resetCounts();
+    topicScore.resetCounts();
+
+    for (int d=0; d<D; d++) {
+
+      int c = docs.getDocument(d).getCluster();
+
+      int[] fs = docs.getDocument(d).getTokens();
+
+      int nd = fs.length;
+
+      for (int i=0; i<nd; i++) {
+
+        int w = fs[i];
+        int j = z[d][i];
+
+        logProb += Math.log(getScore(w, j, d, c));
+
+        wordScore.incrementCounts(w, j);
+        topicScore.incrementCounts(j, d, c);
+      }
+    }
+
+    return logProb;
+  }
+
+  private void sampleTopics(Corpus docs, boolean init) {
+
+    // resample topics
+
+    for (int d=0; d<D; d++) {
+
+      int c = docs.getDocument(d).getCluster();
+
+      int[] fs = docs.getDocument(d).getTokens();
+
+      int nd = fs.length;
+
+      if (init)
+        z[d] = new int[nd];
+
+      for (int i=0; i<nd; i++) {
+
+        int w = fs[i];
+        int oldTopic = z[d][i];
+
+        if (!init) {
+          wordScore.decrementCounts(w, oldTopic);
+          topicScore.decrementCounts(oldTopic, d, c);
+        }
+
+        // build a distribution over topics
+
+        double dist[] = new double[T];
+        double distSum = 0.0;
+
+        for (int j=0; j<T; j++) {
+
+          double score = getScore(w, j, d, c);
+
+          dist[j] = score;
+          distSum += score;
+        }
+
+        int newTopic = rng.nextDiscrete(dist, distSum);
+
+        z[d][i] = newTopic;
+
+        wordScore.incrementCounts(w, newTopic);
+        topicScore.incrementCounts(newTopic, d, c);
+      }
+    }
+  }
+
+  // estimate topics
+
+  public void estimate(Corpus docs, TIntIntHashMap unseenCounts, int T, int C, double alpha, double beta, int numItns, int printInterval, int saveStateInterval, boolean[] sample, String clusterTopicsFileName, String documentTopicsFileName, String topicWordsFileName, String stateFileName, String alphaFileName, String betaFileName, String logProbFileName) {
+
+    Alphabet wordDict = docs.getWordDict();
+
+    assert (saveStateInterval == 0) || (numItns % saveStateInterval == 0);
+
+    rng = new LogRandoms();
+
+    this.T = T;
+    this.C = C;
+
+    W = wordDict.size();
+    D = docs.size();
+
+    System.out.println("Num docs: " + D);
+    System.out.println("Num words in vocab: " + W);
+    System.out.println("Num topics: " + T);
+
+    wordScore = new WordScore(W, T, beta, unseenCounts);
+    topicScore = new ClusterTopicScore(T, D, C, alpha, "minimal");
+
+    z = new int[D][];
+
+    long start = System.currentTimeMillis();
+
+    sampleTopics(docs, true);
+
+    try {
+
+      PrintWriter logProbWriter = new PrintWriter(logProbFileName);
+
+      // count matrices have been populated, every token has been
+      // assigned to a single topic, so Gibbs sampling can start
+
+      for (int s=1; s<=numItns; s++) {
+
+        if (s % 10 == 0)
+          System.out.print(s);
+        else
+          System.out.print(".");
+
+        System.out.flush();
+
+        sampleTopics(docs, false);
+
+        if (sample[0])
+          topicScore.sampleAlpha(docs, z, rng, 5, 1.0);
+
+        if (sample[1])
+          wordScore.sampleBeta(docs, z, rng, 5, 1.0);
+
+        if (printInterval != 0) {
+          if (s % printInterval == 0) {
+            System.out.println();
+            wordScore.print(wordDict, 0.0, 10, null);
+
+            logProbWriter.println(logProb(docs));
+            logProbWriter.flush();
+          }
+        }
+
+        if (saveStateInterval != 0) {
+          if (s % saveStateInterval == 0) {
+            docs.printFeatures(z, stateFileName + "." + s);
+            topicScore.printAlpha(alphaFileName + "." + s);
+            wordScore.printBeta(betaFileName + "." + s);
+          }
+        }
+      }
+
+      Timer.printTimingInfo(start, System.currentTimeMillis());
+
+      if (saveStateInterval == 0) {
+        docs.printFeatures(z, stateFileName);
+        topicScore.printAlpha(alphaFileName);
+        wordScore.printBeta(betaFileName);
+      }
+
+      topicScore.printClusterTopics(clusterTopicsFileName);
+      topicScore.print(docs, documentTopicsFileName);
+      wordScore.print(wordDict, topicWordsFileName);
+    }
+    catch (IOException e) {
+      System.out.println(e);
+    }
+  }
+}
---
+++ src/ClusterTopicScore.java
@@ -0,0 +1,443 @@
+package edu.umass.cs.wallach.cluster;
+
+import java.io.*;
+import java.util.*;
+import java.util.zip.*;
+
+import gnu.trove.*;
+
+import cc.mallet.types.*;
+import cc.mallet.util.Maths;
+
+public class ClusterTopicScore {
+
+  // observed counts
+
+  private int[][] topicDocCounts; // N_{j|d}
+  private int[] topicDocCountsNorm; // N_{.|d}
+
+  private int[][] topicClusterCounts; // N_{j|c}
+  private int[] topicClusterCountsNorm; // N_{.|c}
+
+  private int[] topicCounts; // N_{j}
+  private int topicCountsNorm; // N_{.}
+
+  private int[][] topicClusterCountsTrain;
+  private int[] topicClusterCountsNormTrain;
+
+  private int[] topicCountsTrain;
+  private int topicCountsNormTrain;
+
+  private int T, D, C; // constants
+
+  // hyperparamters
+
+  private double[] alpha;
+
+  private boolean resetToTrain = false;
+
+  private String score;
+
+  // create a score function with zero counts
+
+  public ClusterTopicScore(int T, int D, int C, double alpha, String score) {
+
+    this.T = T;
+    this.D = D;
+    this.C = C;
+
+    this.alpha = new double[3];
+    Arrays.fill(this.alpha, alpha);
+
+    this.score = score;
+
+    // allocate space for counts
+
+    topicDocCounts = new int[T][D];
+    topicDocCountsNorm = new int[D];
+
+    topicClusterCounts = new int[T][C];
+    topicClusterCountsNorm = new int[C];
+
+    topicCounts = new int[T];
+    topicCountsNorm = 0;
+  }
+
+  public double getScore(int j, int d, int c) {
+
+    double score = 1.0 / T;
+
+    int nj = topicCounts[j];
+    int n = topicCountsNorm;
+
+    score *= alpha[0] / (n + alpha[0]);
+    score += nj / (n + alpha[0]);
+
+    int njc = topicClusterCounts[j][c];
+    int nc = topicClusterCountsNorm[c];
+
+    score *= alpha[1] / (nc + alpha[1]);
+    score += njc / (nc + alpha[1]);
+
+    int njd = topicDocCounts[j][d];
+    int nd = topicDocCountsNorm[d];
+
+    score *= alpha[2] / (nd + alpha[2]);
+    score += njd / (nd + alpha[2]);
+
+    return score;
+  }
+
+  public double getScoreNoPrior(int j, int d) {
+
+    int nd = topicDocCountsNorm[d];
+
+    if (nd == 0)
+      return 0.0;
+    else
+      return (double) topicDocCounts[j][d] / (double) nd;
+  }
+
+  public double getClusterScoreNoPrior(int j, int c) {
+
+    int nc = topicClusterCountsNorm[c];
+
+    if (nc == 0)
+      return 0.0;
+    else
+      return (double) topicClusterCounts[j][c] / (double) nc;
+  }
+
+  public void incrementCounts(int j, int d, int c) {
+
+    int oldCount = topicDocCounts[j][d]++;
+    topicDocCountsNorm[d]++;
+
+    if (score.equals("minimal")) {
+      if (oldCount == 0) {
+
+        oldCount = topicClusterCounts[j][c]++;
+        topicClusterCountsNorm[c]++;
+
+        if (oldCount == 0) {
+
+          topicCounts[j]++;
+          topicCountsNorm++;
+        }
+      }
+    }
+    else {
+
+      topicClusterCounts[j][c]++;
+      topicClusterCountsNorm[c]++;
+
+      topicCounts[j]++;
+      topicCountsNorm++;
+    }
+  }
+
+  public void decrementCounts(int j, int d, int c) {
+
+    int oldCount = topicDocCounts[j][d]--;
+    topicDocCountsNorm[d]--;
+
+    if (score.equals("minimal")) {
+      if (oldCount == 1) {
+
+        oldCount = topicClusterCounts[j][c]--;
+        topicClusterCountsNorm[c]--;
+
+        if (oldCount == 1) {
+
+          topicCounts[j]--;
+          topicCountsNorm--;
+        }
+      }
+    }
+    else {
+
+      topicClusterCounts[j][c]--;
+      topicClusterCountsNorm[c]--;
+
+      topicCounts[j]--;
+      topicCountsNorm--;
+    }
+  }
+
+  // this must be called before processing test data
+
+  public void lock(int numTestDocs) {
+
+    this.D = numTestDocs;
+
+    topicDocCounts = new int[T][D];
+    topicDocCountsNorm = new int[D];
+
+    // only need to lock the non-document-specific counts
+
+    topicClusterCountsTrain = topicClusterCounts.clone();
+    topicClusterCountsNormTrain = topicClusterCountsNorm.clone();
+
+    topicCountsTrain = topicCounts.clone();
+    topicCountsNormTrain = topicCountsNorm;
+
+    resetToTrain = true;
+  }
+
+  public void resetCounts() {
+
+    for (int j=0; j<T; j++)
+      Arrays.fill(topicDocCounts[j], 0);
+
+    Arrays.fill(topicDocCountsNorm, 0);
+
+    if (resetToTrain) {
+
+      topicClusterCounts = topicClusterCountsTrain.clone();
+      topicClusterCountsNorm = topicClusterCountsNormTrain.clone();
+
+      topicCounts = topicCountsTrain.clone();
+      topicCountsNorm = topicCountsNormTrain;
+    }
+    else {
+
+      for (int j=0; j<T; j++)
+        Arrays.fill(topicClusterCounts[j], 0);
+
+      Arrays.fill(topicClusterCountsNorm, 0);
+
+      Arrays.fill(topicCounts, 0);
+      topicCountsNorm = 0;
+    }
+  }
+
+  // computes log prob using the predictive distribution
+
+  public double logProb(Corpus docs, int[][] z) {
+
+    double logProb = 0.0;
+
+    resetCounts();
+
+    assert docs.size() == D;
+    assert z.length == D;
+
+    for (int d=0; d<D; d++) {
+
+      int c = docs.getDocument(d).getCluster();
+
+      int[] fs = docs.getDocument(d).getTokens();
+
+      int nd = fs.length;
+
+      for (int i=0; i<nd; i++) {
+
+        int j = z[d][i];
+
+        logProb += Math.log(getScore(j, d, c));
+
+        incrementCounts(j, d, c);
+      }
+    }
+
+    return logProb;
+  }
+
+  public double[] getAlpha() {
+
+    return alpha;
+  }
+
+  public void printAlpha(String fileName) {
+
+    try {
+
+      PrintWriter pw = new PrintWriter(fileName);
+
+      for (int i=0; i<alpha.length; i++)
+        pw.println(alpha[i]);
+
+      pw.close();
+    }
+    catch (IOException e) {
+      System.out.println(e);
+    }
+  }
+
+  private double logProb(Corpus docs, int[][] z, double[] newLogAlpha) {
+
+    double[] oldAlpha = alpha.clone();
+
+    for (int i=0; i<alpha.length; i++)
+      alpha[i] = Math.exp(newLogAlpha[i]);
+
+    double logProb = logProb(docs, z);
+
+    alpha = oldAlpha.clone();
+
+    return logProb;
+  }
+
+  public void sampleAlpha(Corpus docs, int[][] z, LogRandoms rng, int numIterations, double stepSize) {
+
+    int I = alpha.length;
+
+    double[] rawParam = new double[I];
+    double rawParamSum = 0.0;
+
+    for (int i=0; i<I; i++) {
+      rawParam[i] = Math.log(alpha[i]);
+      rawParamSum += rawParam[i];
+    }
+
+    double[] l = new double[I];
+    double[] r = new double[I];
+
+    for (int s=0; s<numIterations; s++) {
+
+      double lp = logProb(docs, z, rawParam) + rawParamSum;
+      double lpNew = Math.log(rng.nextUniform()) + lp;
+
+      for (int i=0; i<I; i++) {
+        l[i] = rawParam[i] - rng.nextUniform() * stepSize;
+        r[i] = l[i] + stepSize;
+      }
+
+      double[] rawParamNew = new double[I];
+      double rawParamNewSum = 0.0;
+
+      while (true) {
+
+        rawParamNewSum = 0.0;
+
+        for (int i=0; i<I; i++) {
+          rawParamNew[i] = l[i] + rng.nextUniform() * (r[i] - l[i]);
+          rawParamNewSum += rawParamNew[i];
+        }
+
+        if (logProb(docs, z, rawParamNew) + rawParamNewSum > lpNew)
+          break;
+        else
+          for (int i=0; i<I; i++)
+            if (rawParamNew[i] < rawParam[i])
+              l[i] = rawParamNew[i];
+            else
+              r[i] = rawParamNew[i];
+      }
+
+      rawParam = rawParamNew;
+      rawParamSum = rawParamNewSum;
+    }
+
+    for (int i=0; i<I; i++)
+      alpha[i] = Math.exp(rawParam[i]);
+  }
+
+  public void print(Corpus docs, String fileName) {
+
+    print(docs, 0.0, -1, fileName);
+  }
+
+  public void print(Corpus docs, double threshold, int numTopics, String fileName) {
+
+    assert docs.size() == D;
+
+    try {
+
+      PrintStream pw = new PrintStream(new GZIPOutputStream(new BufferedOutputStream(new FileOutputStream(new File(fileName)))));
+
+      pw.println("#doc source topic proportion ...");
+
+      Probability[] probs = new Probability[T];
+
+      for (int d=0; d<D; d++) {
+
+        pw.print(d); pw.print(" ");
+        pw.print(docs.getDocument(d).getSource()); pw.print(" ");
+
+        for (int j=0; j<T; j++)
+          probs[j] = new Probability(j, getScore(j, d, c));
+
+        Arrays.sort(probs);
+
+        if ((numTopics > T) || (numTopics < 0))
+          numTopics = T;
+
+        for (int i=0; i<numTopics; i++) {
+
+          // break if there are no more topics whose proportion is
+          // greater than zero or threshold...
+
+          if ((probs[i].prob == 0) || (probs[i].prob < threshold))
+            break;
+
+          pw.print(probs[i].index); pw.print(" ");
+          pw.print(probs[i].prob); pw.print(" ");
+        }
+
+        pw.println();
+      }
+
+      pw.close();
+    }
+    catch (IOException e) {
+      System.out.println(e);
+    }
+  }
+
+  public void print(String fileName) {
+
+    print(0.0, -1, fileName);
+  }
+
+  public void print(double threshold, int numTopics, String fileName) {
+
+    try {
+
+      PrintStream pw = new PrintStream(new GZIPOutputStream(new BufferedOutputStream(new FileOutputStream(new File(fileName)))));
+
+      pw.println("#cluster topic proportion ...");
+
+      Probability[] probs = new Probability[T];
+
+      int clusterNum = 0;
+
+      for (int c=0; c<C; c++) {
+        for (int j=0; j<T; j++)
+          probs[j] = new Probability(j, getClusterScoreNoPrior(j, c));
+
+        Arrays.sort(probs);
+
+        if (probs[0].prob == 0)
+          continue;
+        else {
+
+          pw.print(clusterNum); pw.print(" ");
+          clusterNum++;
+        }
+
+        if ((numTopics > T) || (numTopics < 0))
+          numTopics = T;
+
+        for (int i=0; i<numTopics; i++) {
+
+          // break if there are no more topics whose proportion is
+          // greater than zero or threshold...
+
+          if ((probs[i].prob == 0) || (probs[i].prob < threshold))
+            break;
+
+          pw.print(probs[i].index); pw.print(" ");
+          pw.print(probs[i].prob); pw.print(" ");
+        }
+
+        pw.println();
+      }
+
+      pw.close();
+    }
+    catch (IOException e) {
+      System.out.println(e);
+    }
+  }
+}
