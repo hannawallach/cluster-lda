104fbc1d176a497bd2d667662f852a576d486728
Hanna Wallach <wallach@cs.umass.edu>
2012-03-30 16:30:14 -0400

    Modified scripts to run correctly on swarm.

--- scripts/run_cluster.sh
+++ scripts/run_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: e.g., mkdir -p results/sge; qsub -t 1-5 -cwd -V -o results/sge/stdout_0.5_UP.txt -e results/sge/stderr_0.5_UP.txt ./scripts/run_cluster.sh 0.5 UP
+# Usage: e.g., mkdir -p results/sge; qsub -t 1-5 -cwd -V -o results/sge/stdout_0.5_DP_patents_core_100.txt -e results/sge/stderr_0.5_DP_patents_core_100.txt ./scripts/run_cluster.sh 0.5 DP patents_core 100 results/lda/patents_core/T100-S1000-SAMPLE11-ID1/state.txt.gz.1000
 
 ID=`expr $SGE_TASK_ID`
-make results/cluster_lda/core/C25-SG2000-SC1-THETA$1-SAMPLEfalse-DOCCOUNTStrue-$2-ID${ID} C=25 SG=2000 SC=1 THETA=$1 SAMPLE=false DOCCOUNTS=true PRIOR=$2 ID=${ID}
+make results/cluster_lda/$3/C25-SG2000-SC1-THETA$1-SAMPLEfalse-DOCCOUNTStrue-$2-ID${ID} T=$4 C=25 SG=2000 SC=1 ST=1 THETA=$1 SAMPLE=false DOCCOUNTS=true PRIOR=$2 ID=${ID} STATE_FILE=$5
--- scripts/run_cluster_qsub.sh
+++ scripts/run_cluster_qsub.sh
@@ -4,8 +4,13 @@ RUNS=5
 
 make build
 
+DATA=patents_core
+T=100
+
+STATE_FILE=results/lda/${DATA}/T${T}-S1000-SAMPLE11-ID1/state.txt.gz.1000
+
 for PRIOR in `echo UP PYP DP`; do
   for THETA in `echo 0.25 0.5 1.0 2.0 5.0 10.0 15.0 20.0`; do
-    mkdir -p results/sge; qsub -t 1-${RUNS} -cwd -V -o results/sge/stdout_${THETA}_${PRIOR}.txt -e results/sge/stderr_${THETA}_${PRIOR}.txt ./scripts/run_cluster.sh ${THETA} ${PRIOR}
+    mkdir -p results/sge; qsub -t 1-${RUNS} -cwd -V -o results/sge/stdout_${THETA}_${PRIOR}_${DATA}_${T}.txt -e results/sge/stderr_${THETA}_${PRIOR}_${DATA}_${T}.txt ./scripts/run_cluster.sh ${THETA} ${PRIOR} ${DATA} ${T} ${STATE_FILE}
   done
 done
