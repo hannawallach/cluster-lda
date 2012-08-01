3e156f01fb38344154b630acfbd2bd84f13fb8f9
Hanna Wallach <wallach@cs.umass.edu>
2012-07-31 21:47:09 -0400

    Modified scripts/run_cluster_qsub.sh and scripts/run_cluster.sh.

--- scripts/run_cluster.sh
+++ scripts/run_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: e.g., mkdir -p results/sge; qsub -t 1-5 -cwd -V -o results/sge/stdout_0.5_0.0_DP_patents_core_100.txt -e results/sge/stderr_0.5_0.0_DP_patents_core_100.txt ./scripts/run_cluster.sh 0.5 0.0 DP patents_core 100 results/lda/patents_core/T100-S1000-SAMPLE11-ID1/state.txt.gz.1000
+# Usage: e.g., mkdir -p results/sge; qsub -t 1-5 -cwd -V -o results/sge/stdout_patents_core_100_10000_0.5_0.0_false_DP.txt -e results/sge/stderr_patents_core_100_10000_0.5_0.0_false_DP.txt ./scripts/run_cluster.sh patents_core 100 10000 0.5 0.0 false DP results/lda/patents_core/T100-S1000-SAMPLE11-ID1/state.txt.gz.1000
 
 ID=`expr $SGE_TASK_ID`
-make results/cluster_lda/$4/T$5-C1-SG10000-SC1-ST1-THETA$1-EPS$2-SAMPLEfalse-PERCLUSTERtrue-DOCCOUNTStrue-$3-ID${ID} T=$5 C=1 SG=10000 SC=1 ST=1 THETA=$1 EPS=$2 SAMPLE=false PERCLUSTER=true DOCCOUNTS=true PRIOR=$3 ID=${ID} STATE_FILE=$6
+make results/cluster_lda/$1/T$2-C1-SG$3-SC1-ST1-THETA$4-EPS$5-SAMPLE$6-PERCLUSTERtrue-DOCCOUNTStrue-$7-ID${ID} T=$2 C=1 SG=$3 SC=1 ST=1 THETA=$4 EPS=$5 SAMPLE=$6 PERCLUSTER=true DOCCOUNTS=true PRIOR=$7 ID=${ID} STATE_FILE=$8
--- scripts/run_cluster_qsub.sh
+++ scripts/run_cluster_qsub.sh
@@ -2,18 +2,34 @@
 
 RUNS=1
 
-make build
+SG=10000
+SAMPLE=false
 
-DATA=house_press_1000
+DATA=evening_news
 
-for T in `echo 25 50 75 100 150`; do
+for T in `echo 10 20 30 40 50`; do
 
   STATE_FILE=results/lda/${DATA}/T${T}-S1000-SAMPLE11-ID1/state.txt.gz.1000
 
   for PRIOR in `echo DP`; do
-    for THETA in `echo 0.01 0.1 0.2 0.5 1.0 2.0 5.0 10.0`; do
+    for THETA in `echo 0.1 0.2 0.5 1.0 2.0 5.0 10.0`; do
       for EPS in `echo 0.0`; do
-        mkdir -p results/sge; qsub -l long -t 1-${RUNS} -cwd -V -o results/sge/stdout_${THETA}_${EPS}_${PRIOR}_${DATA}_${T}.txt -e results/sge/stderr_${THETA}_${EPS}_${PRIOR}_${DATA}_${T}.txt ./scripts/run_cluster.sh ${THETA} ${EPS} ${PRIOR} ${DATA} ${T} ${STATE_FILE}
+        mkdir -p results/sge; qsub -l long -t 1-${RUNS} -cwd -V -o results/sge/stdout_${DATA}_${T}_${SG}_${THETA}_${EPS}_${SAMPLE}_${PRIOR}.txt -e results/sge/stderr_${DATA}_${T}_${SG}_${THETA}_${EPS}_${SAMPLE}_${PRIOR}.txt ./scripts/run_cluster.sh ${DATA} ${T} ${SG} ${THETA} ${EPS} ${SAMPLE} ${PRIOR} ${STATE_FILE}
+      done
+    done
+  done
+done
+
+DATA=house_press_1000
+
+for T in `echo 50 100 150 200 250`; do
+
+  STATE_FILE=results/lda/${DATA}/T${T}-S1000-SAMPLE11-ID1/state.txt.gz.1000
+
+  for PRIOR in `echo DP`; do
+    for THETA in `echo 0.1 0.2 0.5 1.0 2.0 5.0 10.0`; do
+      for EPS in `echo 0.0`; do
+        mkdir -p results/sge; qsub -l long -t 1-${RUNS} -cwd -V -o results/sge/stdout_${DATA}_${T}_${SG}_${THETA}_${EPS}_${SAMPLE}_${PRIOR}.txt -e results/sge/stderr_${DATA}_${T}_${SG}_${THETA}_${EPS}_${SAMPLE}_${PRIOR}.txt ./scripts/run_cluster.sh ${DATA} ${T} ${SG} ${THETA} ${EPS} ${SAMPLE} ${PRIOR} ${STATE_FILE}
       done
     done
   done
