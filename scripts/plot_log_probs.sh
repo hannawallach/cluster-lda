b028fdaba0d33791ac12aa81491ffa2d6c68f581
Hanna Wallach <wallach@cs.umass.edu>
2014-01-05 14:59:31 -0500

    Modified scripts/plot_log_probs.sh to be consistent with current version of scripts/run_cluster_qsub.sh.

--- scripts/plot_log_probs.sh
+++ scripts/plot_log_probs.sh
@@ -2,15 +2,31 @@
 
 SCRIPT=`basename $0 | cut -d'.' -f1`.R
 
-RUNS=5
+RUNS=1
 
-DATA=patents_core
-T=100
+SG=10000
+SAMPLE=false
 
-for PRIOR in `echo UP PYP DP`; do
-  for THETA in `echo 0.25 0.5 1.0 2.0 5.0 10.0 15.0 20.0`; do
-    for EPS in `echo 0.0`; do
-      cat scripts/${SCRIPT} | R --slave --vanilla --args results/cluster_lda/${DATA}/T${T}-C25-SG2000-SC1-ST1-THETA${THETA}-EPS${EPS}-SAMPLEfalse-PERCLUSTERfalse-DOCCOUNTStrue-${PRIOR}-ID ${RUNS}
+DATA=evening_news
+
+for T in `echo 10 20 30 40 50`; do
+  for PRIOR in `echo DP`; do
+    for THETA in `echo 0.1 0.2 0.5 1.0 2.0 5.0 10.0`; do
+      for EPS in `echo 0.0`; do
+        cat scripts/${SCRIPT} | R --slave --vanilla --args results/cluster_lda/${DATA}/T${T}-C1-SG${SG}-SC1-ST1-THETA${THETA}-EPS${EPS}-SAMPLE${SAMPLE}-PERCLUSTERtrue-DOCCOUNTStrue-${PRIOR}-ID ${RUNS}
+      done
+    done
+  done
+done
+
+DATA=house_press_1000
+
+for T in `echo 50 100 150 200 250`; do
+  for PRIOR in `echo DP`; do
+    for THETA in `echo 0.1 0.2 0.5 1.0 2.0 5.0 10.0`; do
+      for EPS in `echo 0.0`; do
+        cat scripts/${SCRIPT} | R --slave --vanilla --args results/cluster_lda/${DATA}/T${T}-C1-SG${SG}-SC1-ST1-THETA${THETA}-EPS${EPS}-SAMPLE${SAMPLE}-PERCLUSTERtrue-DOCCOUNTStrue-${PRIOR}-ID ${RUNS}
+      done
     done
   done
 done
